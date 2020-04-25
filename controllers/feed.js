c0e019e6366549a113f83d4a53856916d97d586e
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-25 11:15:44 +0100

    worked on createPost route to receive data from UI and return response

--- controllers/feed.js
+++ controllers/feed.js
@@ -16,13 +16,16 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
+  console.log(title, content);
 
   res.status(201).json({
     message: 'Post created successfully',
     post: {
-      id: new Date().toISOString(),
+      _id: new Date().toISOString(),
       title: title,
       content: content,
+      creator: { name: 'Ademola' },
+      createdAt: new Date(),
     },
   });
 };
