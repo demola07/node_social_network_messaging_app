eff313de08e53d6eeea291e8df3b05de343bc195
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-26 19:58:23 +0100

    Adding User signup validation, encrypting password, and signing users up

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.put('/signup');
+const { body } = require('express-validator');
+const User = require('../models/user');
+
+const authController = require('../controllers/auth');
+
+router.put(
+  '/signup',
+  [
+    body('email', 'Please enter a valid email')
+      .isEmail()
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject('Email address already exists');
+          }
+        });
+      })
+      .normalizeEmail(),
+    body('password').trim().isLength({ min: 5 }),
+    body('name').trim().trim().not().isEmpty(),
+  ],
+  authController.signup
+);
 
 module.exports = router;
