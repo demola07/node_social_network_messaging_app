8340ff9e91167ec9f93f6ce5281b4ea66289ee06
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-26 20:28:01 +0100

    Worked with authentication using jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
-const User = require('../models/user');
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -58,6 +60,15 @@ exports.login = (req, res, next) => {
         error.statusCode = 401;
         throw error;
       }
+      const token = jwt.sign(
+        { email: loadedUser.email, userId: loadedUser._id.toString() },
+        'somesupersecretsecret',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({
+        token: token,
+        userId: loadedUser._id.toString(),
+      });
     })
     .catch((err) => {
       if (!err.statusCode) {
