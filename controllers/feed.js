684b3c7d79f6c263c5dc3e2531b1692fcb2e03c0
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-27 01:24:57 +0100

    Final edit and clean up

--- controllers/feed.js
+++ controllers/feed.js
@@ -34,8 +34,6 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
-  console.log(req.userId);
-
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -110,8 +108,6 @@ exports.getPost = (req, res, next) => {
 };
 
 exports.updatePost = (req, res, next) => {
-  console.log(req.userId);
-
   const postId = req.params.postId;
   const errors = validationResult(req);
 
