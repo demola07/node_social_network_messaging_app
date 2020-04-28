a3d5f979a812af187a4c6298f5f518a69af2a341
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-28 15:29:30 +0100

    Adding a Create Post Mutation

--- graphql/schema.js
+++ graphql/schema.js
@@ -20,6 +20,10 @@ module.exports = buildSchema(`
         status: String!
         posts: [Post!]!
     }
+    type AuthData {
+        token: String!
+        userId: String!
+    }
 
     input UserInputData {
         email: String!
@@ -27,9 +31,10 @@ module.exports = buildSchema(`
         password: String!
     }
 
-    type AuthData {
-        token: String!
-        userId: String!
+    input PostInputData {
+        title: String!
+        content: String!
+        imageUrl: String!
     }
 
     type RootQuery{
@@ -38,6 +43,7 @@ module.exports = buildSchema(`
 
     type RootMutation {
         createUser(userInput: UserInputData ): User!
+        createPost(postInput: PostInputData): Post!
     }
 
     schema {
