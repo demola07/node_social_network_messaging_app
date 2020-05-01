a5ba019cc0810d192560e3f4762882c5284946aa
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-01 10:49:41 +0100

    Edit travis file

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    const error = new Error('Validation failed!!');
+    const error = new Error('Validation failed!!!!');
     error.statusCode = 422;
     error.data = errors.array();
     throw error;
